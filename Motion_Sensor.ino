0a9b8b904dad47d28c2f4e584a25fca8b4c15de6
Ayush Agrawal <agaayush15@gmail.com>
2025-08-21 12:43:34 +0530

    Read Potentiometer Value

--- Motion_Sensor.ino
+++ Motion_Sensor.ino
@@ -1,9 +1,12 @@
-void setup() {
-  // put your setup code here, to run once:
-
+// Task 1 -> Read Potentiometer value
+void setup()
+{
+  pinMode(A0, INPUT);
+  Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+  Serial.prinln(analogRead(A0));
+  delay(100);
 }
